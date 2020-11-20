9625c0eaae0d87c67889a5db041c311e0bd23a3e
unknown <fhirjan@gmail.com>
2020-11-20 21:39:30 +0200

    two

---
+++ Assets/Scripts/Forward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forward : MonoBehaviour
+{
+    private float timetodestroy;
+
+    // Use this for initialization
+    void Start()
+    {
+        //GameObject.rigidbody.constantForce
+        timetodestroy = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timetodestroy = timetodestroy - Time.deltaTime;
+        gameObject.transform.position += transform.forward * Time.deltaTime * 30;
+        if (timetodestroy < 0)
+        {
+            Destroy(gameObject);
+        }
+    }   
+}
---
+++ Assets/Scripts/fire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fire : MonoBehaviour
+{
+    public GameObject blast;
+    public GameObject firePlace;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(blast, firePlace.transform.position, firePlace.transform.rotation);
+        }
+    }
+}
